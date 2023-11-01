f84cc1a4358077f2a549afd7f5c32734e1cd81d2
aravintsamy-converse <aravintsamy@conversedatasolutions.com>
2023-11-02 01:13:31 +0530

    devise_setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users
 root 'home#index'
+
+# devise_scope :user do
+#   root to: "devise/sessions#new"
+# end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+admin = User.create(email: 'admin@gmail.com', name: 'admin', username: 'admin', mobile_number: '1234567890',
+                    address: 'address', password: 'admin@123', role: 'admin')
