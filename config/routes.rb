1b8abf461ecfd4b6b694f97f2fa278ccd12c7b3f
aravintsamy-converse <aravintsamy@conversedatasolutions.com>
2023-11-30 17:38:19 +0530

    complete_ui_of_teacher attendance

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,16 @@ Rails.application.routes.draw do
 
   
 
-   resources :user_attendances, only: [:new, :create, :index , :edit, :update, :destroy]
-   post 'update_all', to: 'user_attendances#update_all'
+
+
+   resources :user_attendances, only: [:index ] do
+    collection do
+      post :update_all
+    end
+  end
+
 
   
-   post 'submit_all_attendances', to: 'user_attendances#submit_all', as: 'submit_all_attendances'
 
   resources :students do
     collection do
