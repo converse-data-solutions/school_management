08050361bd427abd02e90da5aafe09fee18ba464
aravintsamy-converse <aravintsamy@conversedatasolutions.com>
2023-11-30 11:23:50 +0530

    by_using_fnd_or_initilize

--- app/controllers/user_attendances_controller.rb
+++ app/controllers/user_attendances_controller.rb
@@ -9,7 +9,7 @@ class UserAttendancesController < ApplicationController
   def create
     @attendance = Attendance.new(attendance_params)
     if @attendance.save
-      redirect_to user_attendances_path
+      redirect_to user_attendances_path(date: @attendance.date)
     else
       render :new
     end
@@ -23,23 +23,33 @@ class UserAttendancesController < ApplicationController
     @attendance = Attendance.find(params[:id])
     if @attendance
       @attendance.update(attendance_params)
-      redirect_to user_attendances_path
+      redirect_to user_attendances_path(date: @attendance.date)
     else
       flash[:error] = 'Attendance record not found.'
       redirect_to user_attendances_path
     end
   end
+  def update_all
+    # Use strong parameters to permit necessary attributes
+    attendances_params = params.require(:attendances).permit!
+
+    attendances_params.each do |user_id, attendance_params|
+      user = User.find(attendance_params[:attendable_id])
+      attendance = Attendance.find_or_initialize_by(attendable_id: user.id, attendable_type: user.class.name, date: attendance_params[:date])
+      attendance.update(attendance_params)
+    end
+
+    # Redirect or render as needed
+    redirect_to user_attendances_path
+  end
 
   def index
     @users = User.where(role: 'staff')
-    @selected_date = params[:date] ? Date.parse(params[:date]) : Date.today
+    @selected_date = params[:date]
+
   end
 
   private
-
-  def set_user
-    @user = User.all
-  end
 
   def attendance_params
     params.require(:attendance).permit(:date, :status, :attendable_id, :attendable_type)
