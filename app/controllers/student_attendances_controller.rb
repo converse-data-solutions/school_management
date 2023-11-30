1fc22c644f4fe8c2eeb9f97ce4929653a792c446
aravintsamy-converse <aravintsamy@conversedatasolutions.com>
2023-11-30 19:51:03 +0530

    partially-filterd_by_section

--- app/controllers/student_attendances_controller.rb
+++ app/controllers/student_attendances_controller.rb
@@ -1,5 +1,5 @@
 class StudentAttendancesController < ApplicationController
-  before_action :set_student
+  # before_action :set_student
 
   def new
     @attendance = @student.attendances.new
@@ -29,6 +29,30 @@ class StudentAttendancesController < ApplicationController
     end
   end
 
+  def update_all
+    # Use strong parameters to permit necessary attributes
+    attendances_params = params.require(:attendances).permit!
+
+    attendances_params.each do |_student_id, attendance_params|
+      student = Student.find(attendance_params[:attendable_id])
+      attendance = Attendance.find_or_initialize_by(attendable_id: student.id, attendable_type: student.class.name,
+                                                    date: attendance_params[:date])
+      attendance.update(attendance_params)
+    end
+
+    # Redirect or render as needed
+    redirect_to student_attendances_path
+  end
+
+  def index
+    
+    @selected_date = params[:date]
+    @selected_standard = params[:standard_id]
+    @selected_section = params[:section_id]
+    @students = Student.where(section_id: @selected_section)
+   
+  end
+
   private
 
   def set_student
@@ -39,4 +63,3 @@ class StudentAttendancesController < ApplicationController
     params.require(:attendance).permit(:date, :status)
   end
 end
-
