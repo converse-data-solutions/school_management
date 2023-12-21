4b34f3c48444902c65bcd12b90f4147c1950eb46
aravintsamy-converse <aravintsamy@conversedatasolutions.com>
2023-12-21 17:46:51 +0530

    complete_staff_attendances

--- app/javascript/controllers/staff_notice_calendar.js
+++ app/javascript/controllers/staff_notice_calendar.js
@@ -1,25 +1,23 @@
 function initValidate() {
-
   console.log("entered notice");
-  var calendarForStaffNotice = document.getElementById('staff-notice-calendar');
-
-
+  var calendarForStaffNotice = document.getElementById("staff-notice-calendar");
+  if (!calendarForStaffNotice) {
+    return;
+  }
 
   var staffNoticeCalendar = new FullCalendar.Calendar(calendarForStaffNotice, {
     headerToolbar: {
-      left: 'prev,next today',
-      center: 'title',
-      right: ' dayGridMonth'  
+      left: "prev,next today",
+      center: "title",
+      right: " dayGridMonth",
     },
-    
+
     initialDate: new Date(),
     navLinks: true, // can click day/week names to navigate views
     businessHours: true, // display business hours
     editable: true,
     selectable: true,
     events: [],
-
-   
   });
 
   staffNoticeCalendar.render();
