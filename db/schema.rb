f576e8ce1af3ee1cd84c8a85a87e7a3795c8ed7e
aravintsamy-converse <aravintsamy@conversedatasolutions.com>
2023-12-28 09:53:10 +0530

    retify_the_migration_isssu

--- db/schema.rb
+++ db/schema.rb
@@ -10,143 +10,144 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_27_120556) do
-  create_table "academic_details", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.integer "roll_no"
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer "admission_no"
-    t.bigint "student_id", null: false
-    t.string "section_name"
-    t.string "standard_name"
-    t.string "academic_year"
-    t.index ["student_id"], name: "index_academic_details_on_student_id"
+ActiveRecord::Schema[7.1].define(version: 20_231_227_120_556) do
+  create_table 'academic_details', charset: 'utf8mb4', collation: 'utf8mb4_0900_ai_ci', force: :cascade do |t|
+    t.integer 'roll_no'
+    t.string 'name'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.integer 'admission_no'
+    t.bigint 'student_id', null: false
+    t.string 'section_name'
+    t.string 'standard_name'
+    t.string 'academic_year'
+    t.index ['student_id'], name: 'index_academic_details_on_student_id'
   end
 
-  create_table "active_storage_attachments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "record_type", null: false
-    t.bigint "record_id", null: false
-    t.bigint "blob_id", null: false
-    t.datetime "created_at", null: false
-    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
-    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  create_table 'active_storage_attachments', charset: 'utf8mb4', collation: 'utf8mb4_0900_ai_ci', force: :cascade do |t|
+    t.string 'name', null: false
+    t.string 'record_type', null: false
+    t.bigint 'record_id', null: false
+    t.bigint 'blob_id', null: false
+    t.datetime 'created_at', null: false
+    t.index ['blob_id'], name: 'index_active_storage_attachments_on_blob_id'
+    t.index %w[record_type record_id name blob_id], name: 'index_active_storage_attachments_uniqueness',
+                                                    unique: true
   end
 
-  create_table "active_storage_blobs", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.string "key", null: false
-    t.string "filename", null: false
-    t.string "content_type"
-    t.text "metadata"
-    t.string "service_name", null: false
-    t.bigint "byte_size", null: false
-    t.string "checksum"
-    t.datetime "created_at", null: false
-    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  create_table 'active_storage_blobs', charset: 'utf8mb4', collation: 'utf8mb4_0900_ai_ci', force: :cascade do |t|
+    t.string 'key', null: false
+    t.string 'filename', null: false
+    t.string 'content_type'
+    t.text 'metadata'
+    t.string 'service_name', null: false
+    t.bigint 'byte_size', null: false
+    t.string 'checksum'
+    t.datetime 'created_at', null: false
+    t.index ['key'], name: 'index_active_storage_blobs_on_key', unique: true
   end
 
-  create_table "active_storage_variant_records", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.bigint "blob_id", null: false
-    t.string "variation_digest", null: false
-    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  create_table 'active_storage_variant_records', charset: 'utf8mb4', collation: 'utf8mb4_0900_ai_ci',
+                                                 force: :cascade do |t|
+    t.bigint 'blob_id', null: false
+    t.string 'variation_digest', null: false
+    t.index %w[blob_id variation_digest], name: 'index_active_storage_variant_records_uniqueness', unique: true
   end
 
-  create_table "attendances", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.date "date"
-    t.integer "status"
-    t.string "attendable_type", null: false
-    t.bigint "attendable_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "color"
-    t.bigint "created_by"
-    t.index ["attendable_type", "attendable_id"], name: "index_attendances_on_attendable"
+  create_table 'attendances', charset: 'utf8mb4', collation: 'utf8mb4_0900_ai_ci', force: :cascade do |t|
+    t.date 'date'
+    t.integer 'status'
+    t.string 'attendable_type', null: false
+    t.bigint 'attendable_id', null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.string 'color'
+    t.bigint 'created_by'
+    t.index %w[attendable_type attendable_id], name: 'index_attendances_on_attendable'
   end
 
-  create_table "notices", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.string "notice_type"
-    t.string "notice"
-    t.date "notice_date"
-    t.bigint "user_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_notices_on_user_id"
+  create_table 'notices', charset: 'utf8mb4', collation: 'utf8mb4_0900_ai_ci', force: :cascade do |t|
+    t.string 'notice_type'
+    t.string 'notice'
+    t.date 'notice_date'
+    t.bigint 'user_id', null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['user_id'], name: 'index_notices_on_user_id'
   end
 
-  create_table "schools", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.string "school_name"
-    t.string "school_address1"
-    t.string "school_address2"
-    t.string "school_email"
-    t.bigint "school_phone"
-    t.string "academic_year"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.boolean "singleton_guard"
+  create_table 'schools', charset: 'utf8mb4', collation: 'utf8mb4_0900_ai_ci', force: :cascade do |t|
+    t.string 'school_name'
+    t.string 'school_address1'
+    t.string 'school_address2'
+    t.string 'school_email'
+    t.bigint 'school_phone'
+    t.string 'academic_year'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.boolean 'singleton_guard'
   end
 
-  create_table "sections", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.string "section_name"
-    t.bigint "standard_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["standard_id"], name: "index_sections_on_standard_id"
+  create_table 'sections', charset: 'utf8mb4', collation: 'utf8mb4_0900_ai_ci', force: :cascade do |t|
+    t.string 'section_name'
+    t.bigint 'standard_id', null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['standard_id'], name: 'index_sections_on_standard_id'
   end
 
-  create_table "standards", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer "fee"
+  create_table 'standards', charset: 'utf8mb4', collation: 'utf8mb4_0900_ai_ci', force: :cascade do |t|
+    t.string 'name'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.integer 'fee'
   end
 
-  create_table "students", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.integer "admission_no"
-    t.string "roll_no"
-    t.string "name"
-    t.string "father_name"
-    t.string "mother_name"
-    t.bigint "mobile_number"
-    t.string "address"
-    t.date "date_of_birth"
-    t.string "gender"
-    t.date "date_of_admission"
-    t.bigint "section_id", null: false
-    t.bigint "user_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "status"
-    t.boolean "deleted", default: false
-    t.index ["section_id"], name: "index_students_on_section_id"
-    t.index ["user_id"], name: "index_students_on_user_id"
+  create_table 'students', charset: 'utf8mb4', collation: 'utf8mb4_0900_ai_ci', force: :cascade do |t|
+    t.integer 'admission_no'
+    t.string 'roll_no'
+    t.string 'name'
+    t.string 'father_name'
+    t.string 'mother_name'
+    t.bigint 'mobile_number'
+    t.string 'address'
+    t.date 'date_of_birth'
+    t.string 'gender'
+    t.date 'date_of_admission'
+    t.bigint 'section_id', null: false
+    t.bigint 'user_id', null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.string 'status'
+    t.index ['section_id'], name: 'index_students_on_section_id'
+    t.index ['user_id'], name: 'index_students_on_user_id'
   end
 
-  create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.string "name"
-    t.string "username"
-    t.bigint "mobile_number"
-    t.string "address"
-    t.string "profession"
-    t.string "gender"
-    t.string "role"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "deleted", default: "0"
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-    t.index ["username"], name: "index_users_on_username", unique: true
+  create_table 'users', charset: 'utf8mb4', collation: 'utf8mb4_0900_ai_ci', force: :cascade do |t|
+    t.string 'email', default: '', null: false
+    t.string 'encrypted_password', default: '', null: false
+    t.string 'reset_password_token'
+    t.datetime 'reset_password_sent_at'
+    t.datetime 'remember_created_at'
+    t.string 'name'
+    t.string 'username'
+    t.bigint 'mobile_number'
+    t.string 'address'
+    t.string 'profession'
+    t.string 'gender'
+    t.string 'role'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.string 'deleted', default: '0'
+    t.index ['email'], name: 'index_users_on_email', unique: true
+    t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
+    t.index ['username'], name: 'index_users_on_username', unique: true
   end
 
-  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "notices", "users"
-  add_foreign_key "sections", "standards"
-  add_foreign_key "students", "sections"
-  add_foreign_key "students", "users"
+  add_foreign_key 'active_storage_attachments', 'active_storage_blobs', column: 'blob_id'
+  add_foreign_key 'active_storage_variant_records', 'active_storage_blobs', column: 'blob_id'
+  add_foreign_key 'notices', 'users'
+  add_foreign_key 'sections', 'standards'
+  add_foreign_key 'students', 'sections'
+  add_foreign_key 'students', 'users'
 end
