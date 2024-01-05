90715e06046b0e97256c7f292cf79e377d6cd472
aravintsamy-converse <aravintsamy@conversedatasolutions.com>
2024-01-05 23:35:43 +0530

    payment_model_done_refactor

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,11 +1,25 @@
 class PaymentsController < ApplicationController
   layout 'pdf'
 
+  before_action :set_invoice_data, only: [:invoice]
+
   def index
     @payments = Payment.all
   end
 
   def invoice
+    respond_to do |format|
+      format.html
+      format.pdf do
+        render pdf: 'Invoice', template: 'payments/invoice', format: [:html], layout: 'pdf', disposition: 'inline',
+               show_as_html: params[:html] == 'true'
+      end
+    end
+  end
+
+  private
+
+  def set_invoice_data
     @payment = Payment.find_by(id: params[:id])
     @academic_fee = @payment.academic_fee
     @academic_detail = @academic_fee.academic_detail
@@ -13,12 +27,5 @@ class PaymentsController < ApplicationController
     @section = @student.section
     @standard = @section.standard
     @school = School.last
-    respond_to do |format|
-      format.html
-      format.pdf do
-        render pdf: 'Invoice', template: 'payments/invoice', format: [:html], layout: 'pdf', disposition: 'inline', show_as_html: params[:html] == 'true'
-      end
-    end
   end
-  
 end
