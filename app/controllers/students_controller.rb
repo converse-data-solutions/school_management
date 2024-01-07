aa896db7bcc0718f55ddf919d968fca1c24b56a0
aravintsamy-converse <aravintsamy@conversedatasolutions.com>
2024-01-07 13:21:49 +0530

    changed_params

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -101,7 +101,7 @@ class StudentsController < ApplicationController
 
   def student_params
     params.require(:student).permit(:admission_no, :roll_no, :name, :father_name, :mother_name, :mobile_number,
-                                    :address, :date_of_birth, :gender, :date_of_admission, :section_id, :user_id, :image, :deleted)
+                                    :address, :date_of_birth, :gender, :date_of_admission, :section_id, :user_id, :image)
   end
 
   def selected_sections
