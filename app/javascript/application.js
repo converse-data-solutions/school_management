97e130964176bd7cc7fa9fd3c39f7a158b28ea41
aravintsamy-converse <aravintsamy@conversedatasolutions.com>
2024-01-07 12:44:59 +0530

    complete_button_hover

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -7,90 +7,90 @@ import flatpickr from "flatpickr";
 import DataTable from "datatables.net-dt";
 
 function initValidate() {
-  function openDropdown(dropdown) {
-    const dropdownContent = dropdown.parent().find(".sub");
-    dropdownContent.slideDown("fast");
-  }
-
-  // Check if the openDropDown class is present and open the dropdown accordingly
-  if ($("#dropdownNavbarManageFee").hasClass("openDropDown")) {
-    const dropdownFee = $("#dropdownNavbarManageFee");
-    openDropdown(dropdownFee);
-  }
-
-  if ($("#dropdownNavbarmanageStudent").hasClass("openDropDown")) {
-    const dropdownStudent = $("#dropdownNavbarmanageStudent");
-    openDropdown(dropdownStudent);
-  }
-
-  if ($("#dropdownNavbarManageParent").hasClass("openDropDown")) {
-    const dropdownParent = $("#dropdownNavbarManageParent");
-    openDropdown(dropdownParent);
-  }
-
-  if ($("#dropdownNavbarManageStaff").hasClass("openDropDown")) {
-    const dropdownStaff = $("#dropdownNavbarManageStaff");
-    openDropdown(dropdownStaff);
-  }
-
-  //accordian for all models
-  $(".accordian-header").click(function () {
-    $(this).toggleClass("active");
-  });
-
-  $(".activebar").on("click", function (e) {
-    $(".activebar").removeClass("activenav");
-    $(this).addClass("activenav");
-  });
-
-  setTimeout(function () {
-    $("#notice").fadeOut();
-  }, 5000);
-
-  $(".main").click(function () {
-    const el = $(this).parent().find(".sub");
-    el.slideToggle("slow");
-
-    if ($(this).hasClass("openDropDown")) {
-      // Hide the next ul list
-      $(this).next("ul").toggleClass("hidden");
+    function openDropdown(dropdown) {
+        const dropdownContent = dropdown.parent().find(".sub");
+        dropdownContent.slideDown("fast");
     }
-  });
 
-  $(document).on("click", function (event) {
-    if (
-      !$(event.target).closest(
-        ".theme-opener, .theme-content, .admin-opener, .admin-content, .more-opener, .more-content"
-      ).length
-    ) {
-      $(".theme-content, .admin-content, .more-content").hide();
-      $(".more-opener").removeClass("activedropdown");
-      $(".admin-opener").removeClass("active-admin");
+    // Check if the openDropDown class is present and open the dropdown accordingly
+    if ($("#dropdownNavbarManageFee").hasClass("openDropDown")) {
+        const dropdownFee = $("#dropdownNavbarManageFee");
+        openDropdown(dropdownFee);
     }
-  });
 
-  $(".theme-opener").click(function () {
-    $(".theme-content").toggle();
-    $(".admin-content, .more-content").hide();
-  });
+    if ($("#dropdownNavbarmanageStudent").hasClass("openDropDown")) {
+        const dropdownStudent = $("#dropdownNavbarmanageStudent");
+        openDropdown(dropdownStudent);
+    }
 
-  $(".admin-opener").click(function () {
-    $(".admin-content").toggle();
-    $(".theme-content, .more-content").hide();
-    $(".admin-opener").toggleClass("active-admin");
-  });
+    if ($("#dropdownNavbarManageParent").hasClass("openDropDown")) {
+        const dropdownParent = $("#dropdownNavbarManageParent");
+        openDropdown(dropdownParent);
+    }
 
-  $(".more-opener").click(function () {
-    $(".more-content").toggle();
-    $(".theme-content, .admin-content").hide();
-    $(".more-opener").toggleClass("activedropdown");
-  });
+    if ($("#dropdownNavbarManageStaff").hasClass("openDropDown")) {
+        const dropdownStaff = $("#dropdownNavbarManageStaff");
+        openDropdown(dropdownStaff);
+    }
+
+    //accordian for all models
+    $(".accordian-header").click(function() {
+        $(this).toggleClass("active");
+    });
+
+    $(".activebar").on("click", function(e) {
+        $(".activebar").removeClass("activenav");
+        $(this).addClass("activenav");
+    });
+
+    setTimeout(function() {
+        $("#notice").fadeOut();
+    }, 5000);
+
+    $(".main").click(function() {
+        const el = $(this).parent().find(".sub");
+        el.slideToggle("slow");
+
+        if ($(this).hasClass("openDropDown")) {
+            // Hide the next ul list
+            $(this).next("ul").toggleClass("hidden");
+        }
+    });
+
+    $(document).on("click", function(event) {
+        if (
+            !$(event.target).closest(
+                ".theme-opener, .theme-content, .admin-opener, .admin-content, .more-opener, .more-content"
+            ).length
+        ) {
+            $(".theme-content, .admin-content, .more-content").hide();
+            $(".more-opener").removeClass("activedropdown");
+            $(".admin-opener").removeClass("active-admin");
+        }
+    });
+
+    $(".theme-opener").click(function() {
+        $(".theme-content").toggle();
+        $(".admin-content, .more-content").hide();
+    });
+
+    $(".admin-opener").click(function() {
+        $(".admin-content").toggle();
+        $(".theme-content, .more-content").hide();
+        $(".admin-opener").toggleClass("active-admin");
+    });
+
+    $(".more-opener").click(function() {
+        $(".more-content").toggle();
+        $(".theme-content, .admin-content").hide();
+        $(".more-opener").toggleClass("activedropdown");
+    });
 }
 
-$(document).ready(function () {
-  initValidate();
-
-  $(document).on("turbo:render", function () {
+$(document).ready(function() {
     initValidate();
-  });
+
+    $(document).on("turbo:render", function() {
+        initValidate();
+    });
 });
