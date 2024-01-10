52c240ecd93845f8433e07805b30ecb14a19dc40
aravintsamy-converse <aravintsamy@conversedatasolutions.com>
2024-01-10 16:50:57 +0530

    moved_filter_logic_to_model

--- app/models/academic_fee.rb
+++ app/models/academic_fee.rb
@@ -15,6 +15,25 @@ class AcademicFee < ApplicationRecord
     Section.find_by(id: params[:section_id]).students.select(:id, :name)
   end
 
+  def self.find_academic_fee(params)
+    return unless (params[:student_id].present? || params[:admission_no].present?) && params[:academic_year].present?
+
+    key = params[:student_id].present? ? :student_id : :admission_no
+    academic_detail = AcademicDetail.find_by({ key => params[key], academic_year: params[:academic_year] })
+    return nil unless academic_detail
+
+    fee = Standard.find_by(id: academic_detail.standard_id)&.fee
+    academic_fee = AcademicFee.find_by(academic_detail_id: academic_detail.id)
+    payments = AcademicFee.includes(:payments).where(academic_detail_id: academic_detail.id)
+
+    {
+      academic_detail:,
+      fee:,
+      academic_fee:,
+      payments:
+    }
+  end
+
   private
 
   def set_discount
