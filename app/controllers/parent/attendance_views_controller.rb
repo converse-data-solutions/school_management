beabe12a0ca655862bcb661519d015e5e5d87bf1
aravintsamy-converse <aravintsamy@conversedatasolutions.com>
2024-02-01 18:07:02 +0530

    partially_done_student_attendance

--- app/controllers/parent/attendance_views_controller.rb
+++ app/controllers/parent/attendance_views_controller.rb
@@ -3,12 +3,11 @@
 # Parent::ParentNoticesController
 class Parent::AttendanceViewsController < ApplicationController # rubocop:disable Style/ClassAndModuleChildren
   include AuthorizationParentHelper
-  before_action :check_user_role
   # GET /parent/parent_attendance_views or /parent/parent_attendance_views.json
   def index
-    @student = params[:student_id]
+    @student_id = params[:student_id]
     @selected_date = params[:selected_date]
-    @attendances = Attendance.where(attendable_id: @student, date: @selected_date)
+    @attendances = Attendance.where(attendable_id: @student_id, date: @selected_date)
     respond_to do |format|
       format.json { render json: @attendances }
       format.html { render :index }
--- app/views/parent/attendance_views/_parent_attendance_view.json.jbuilder
+++ app/views/parent/attendance_views/_parent_attendance_view.json.jbuilder
@@ -1,2 +1,3 @@
-json.extract! parent_parent_attendance_view, :id, :attendable_type, :attendable_id, :date, :status, :created_at, :updated_at
+json.extract! parent_parent_attendance_view, :id, :attendable_type, :attendable_id, :date, :status, :created_at,
+              :updated_at
 json.url parent_parent_attendance_view_url(parent_parent_attendance_view, format: :json)
